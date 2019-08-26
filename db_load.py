a9f68322bdd5d7c94509b119925bcc63cafb8406
Omari Blockton <oblockton@gmail.com>
2019-08-25 21:53:49 -0700

    use request method to pull nasdaq.com html tables

--- db_load.py
+++ db_load.py
@@ -104,7 +104,12 @@ def init_newstable(responseitem):
 def marketstats(tablechoice):
 
     url= 'https://www.nasdaq.com/markets/most-active.aspx'
-    data = pd.read_html(url)
+    header = {
+      "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.75 Safari/537.36",
+      "X-Requested-With": "XMLHttpRequest"
+    }
+    r = requests.get(url, headers=header)
+    data = pd.read_html(r.text)
     mostactive_sharevol = data[2]
     mostactive_sharevol = mostactive_sharevol.drop(['Company.1'], axis=1).to_json(orient = 'records')
     most_declined = data[4]
